1f193db2faec761c9b1c274082e0abfaee4b8b6c
kaou1610 <minhphuong16px@gmail.com>
2021-08-04 23:07:28 +0700

    upload2

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	
 )
+
+func calculate(o string, a, b float64) {
+	switch o {
+	case "add": 
+		fmt.Println("a + b = ", a + b)
+	case "sub":
+		fmt.Println("a - b = ", a - b)
+	case "mul":
+		fmt.Println("a*b = ", a*b)
+	case "div":
+		if b == 0 {
+			fmt.Print("Error")
+		} else {
+			fmt.Println("a/b = ", a/b)
+		}
+		
+	}
+}
 
 func main()  {
 	fmt.Println("Hello world!")
+	calculate("mul", 5, 4)
 }
